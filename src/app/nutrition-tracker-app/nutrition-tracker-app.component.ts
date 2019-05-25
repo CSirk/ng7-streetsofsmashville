75e1a22769b261eb59dfa6561dcf346d6c171302
Cody Sirk <csirk005@gmail.com>
2019-05-25 09:12:50 -0500

    user record dropdown working

--- src/app/nutrition-tracker-app/nutrition-tracker-app.component.ts
+++ src/app/nutrition-tracker-app/nutrition-tracker-app.component.ts
@@ -25,9 +25,6 @@ export class NutritionTrackerAppComponent {
     this.userSavedRecords.push(
       Apple,
       Orange
-        // {RecordName: "Orange", Nutrients:{ Calories: 200, Fat: 0, SaturatedFat: 0, Sodium: 0, Carbohydrates: 0, Cholesterol: 0, Sugar: 0, Fiber: 0, Protein: 0, Potassium: 0, VitaminA: 0, VitaminB1: 0, VitaminB2: 0, VitaminB3: 0, VitaminB4: 0, VitaminB6: 0, VitaminB12: 0, VitaminC: 0, VitaminD: 0, VitaminE: 0, VitaminH: 0, VitaminK: 0, FolicAcid: 0, Calcium: 0, Iron: 0, Chromium: 0, Caffeine: 0, Water: 0, Zinc: 0, Magnesium: 0},
-        // {RecordName: "Apple", Calories: 300, Fat: 0, SaturatedFat: 0, Sodium: 0, Carbohydrates: 0, Cholesterol: 0, Sugar: 0, Fiber: 0, Protein: 0, Potassium: 0, VitaminA: 0, VitaminB1: 0, VitaminB2: 0, VitaminB3: 0, VitaminB4: 0, VitaminB6: 0, VitaminB12: 0, VitaminC: 0, VitaminD: 0, VitaminE: 0, VitaminH: 0, VitaminK: 0, FolicAcid: 0, Calcium: 0, Iron: 0, Chromium: 0, Caffeine: 0, Water: 0, Zinc: 0, Magnesium: 0},
-        // {RecordName: "Grape", Calories: 400, Fat: 0, SaturatedFat: 0, Sodium: 0, Carbohydrates: 0, Cholesterol: 0, Sugar: 0, Fiber: 0, Protein: 0, Potassium: 0, VitaminA: 0, VitaminB1: 0, VitaminB2: 0, VitaminB3: 0, VitaminB4: 0, VitaminB6: 0, VitaminB12: 0, VitaminC: 0, VitaminD: 0, VitaminE: 0, VitaminH: 0, VitaminK: 0, FolicAcid: 0, Calcium: 0, Iron: 0, Chromium: 0, Caffeine: 0, Water: 0, Zinc: 0, Magnesium: 0},
       )
 
     //this.nutrients = NUTRIENTS;
@@ -36,6 +33,15 @@ export class NutritionTrackerAppComponent {
 
   public selectNutritionRecord(record: NutritionRecord) {
     console.log(record)
+    this.currentRecord = record;
+    console.log(this.currentRecord)
+  }
+
+  public userSavedRecordSelectChange (event: any) {
+    //update the ui
+    let pos = this.userSavedRecords.map(function(e) { return e.RecordName; }).indexOf(event.target.value);
+    let record = this.userSavedRecords[pos];
+
     this.currentRecord = record;
   }
 }
